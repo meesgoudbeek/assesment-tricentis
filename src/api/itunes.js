8a4c5ef4db78435c1776d33313bc50834c9b978e
Mees Goudbeek <meesgoudbeek@protonmail.com>
2022-03-16 16:27:00 +0100

    added api fetch call

--- src/api/itunes.js
+++ src/api/itunes.js
@@ -1,1 +1,25 @@
 const endpoint = "https://itunes.apple.com/search?term=";
+
+export function fetchAlbums(searchTerm = "") {
+  return fetch(`${endpoint}${searchTerm}`)
+    .then(function handleResponse(response) {
+      if (!response.ok) throw Error("Request not successful");
+
+      return response.json();
+    })
+    .then(function handleData(data) {
+      if (data?.results?.length > 0) {
+        return data.results
+          .map(function getAlbumNames(result) {
+            return result.collectionName;
+          })
+          .filter(function isUniqueAndNotEmpty(albumName, index, self) {
+            return Boolean(albumName) && self.indexOf(albumName) === index;
+          })
+          .sort(function isAlphabeticallyBigger(albumNameA, albumNameB) {
+            return albumNameA.localeCompare(albumNameB);
+          })
+          .slice(0, 5);
+      }
+    });
+}
